e060104ae4ec22fe266437fb4edebf0f1f21ac34
Heechul Kim <jijisa@iorchard.co.kr>
2023-02-20 21:50:27 +0900

    add krew plugin; add bash-completion

--- docker/README.rst
+++ docker/README.rst
@@ -6,7 +6,17 @@ BTX is a Toolbox image for Burrito platform.
 Build (Optional)
 -------------------
 
-The default version of clients is openstack yoga release.
+Copy .env.sample to .env and edit it.::
+
+   $ cp .env.sample .env
+   $ vi .env
+   TINI_VERSION="v0.19.0"
+   CIRROS_VERSION="0.5.1"
+   OPENSTACK_RELEASE="yoga"
+   CEPH_RELEASE="quincy"
+   K8S_VERSION="v1.24.8"
+   HELM_VERSION="v3.11.1"
+   KREW_VERSION="v0.4.3"
 
 Build the image.::
 
--- docker/build.sh
+++ docker/build.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
-# build jijisa/btx
 set -e 
 
-REL=${1:-yoga}
+. .env
 
 cat <<EOF > openstack.list
-deb http://osbpo.debian.net/osbpo bullseye-${REL}-backports main
-deb http://osbpo.debian.net/osbpo bullseye-${REL}-backports-nochange main
+deb http://osbpo.debian.net/osbpo bullseye-${OPENSTACK_RELEASE}-backports main
+deb http://osbpo.debian.net/osbpo bullseye-${OPENSTACK_RELEASE}-backports-nochange main
 EOF
 
-HELM_VERSION="v3.10.2"
+cat <<EOF > ceph.list
+deb https://download.ceph.com/debian-${CEPH_RELEASE} bullseye main
+EOF
+
 HELM_URL="https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz"
 curl -sL ${HELM_URL} | tar --strip-components 1 -xz linux-amd64/helm
 
-docker build -t jijisa/btx:${REL} .
+KREW_URL="https://github.com/kubernetes-sigs/krew/releases/download/${KREW_VERSION}/krew-linux_amd64.tar.gz"
+curl -sL ${KREW_URL} | tar -xz ./krew-linux_amd64
 
-docker tag jijisa/btx:${REL} jijisa/btx:latest
-docker push jijisa/btx:${REL}
+docker build \
+  -t jijisa/btx:${OPENSTACK_RELEASE}-${K8S_VERSION}-${CEPH_RELEASE} \
+  --build-arg TINI_VERSION=${TINI_VERSION} \
+  --build-arg CIRROS_VERSION=${CIRROS_VERSION} \
+  --build-arg K8S_VERSION=${K8S_VERSION} \
+  --build-arg KREW_VERSION=${KREW_VERSION} \
+  .
+
+docker tag jijisa/btx:${OPENSTACK_RELEASE}-${K8S_VERSION}-${CEPH_RELEASE} \
+  jijisa/btx:latest
+docker push jijisa/btx:${OPENSTACK_RELEASE}-${K8S_VERSION}-${CEPH_RELEASE}
 docker push jijisa/btx:latest
